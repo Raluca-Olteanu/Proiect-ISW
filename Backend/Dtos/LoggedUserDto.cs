df48a6c3850d52b1d9eb52b878ebf357c6972c54
Olteanu Raluca <ralucaolteanu18@yahoo.com>
2024-04-02 22:02:39 +0300

    Dtos added for User

---
+++ Backend/Dtos/LoggedUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend.Dtos
+{
+    public class LoggedUserDto
+    {
+        public string Username { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ Backend/Dtos/RegisterDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend.Dtos
+{
+    public class RegisterDto
+    {
+        public string Username { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string Email { get; set; }
+    }
+}
