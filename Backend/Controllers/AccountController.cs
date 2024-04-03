8fe27b857ae8d493831e430c5ea43696a23251f4
Olteanu Raluca <ralucaolteanu18@yahoo.com>
2024-04-03 13:50:02 +0300

    Added controllers for account and user

---
+++ Backend/Controllers/AccountController.cs
@@ -0,0 +1,86 @@
+﻿using Backend.Data;
+using Backend.Dtos;
+using Backend.Entities;
+using Backend.Helpers;
+using Backend.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private readonly ITokenService _tokenService;
+
+        public AccountController(DataContext context, ITokenService tokenService)
+        {
+            _context = context;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<LoggedUserDto>> Register(RegisterDto registerDto)
+        {
+            if (await UserExists(registerDto.Email))
+                return BadRequest("Email is taken");
+
+            using var hmac = new HMACSHA512();
+            var user = new User
+            {
+                Username = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key,
+                Firstname = registerDto.Firstname,
+                Lastname = registerDto.Lastname,
+                Email = registerDto.Email,
+                Role = registerDto.Role
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return new LoggedUserDto
+            {
+                Username = user.Username,
+                Token = _tokenService.CreateToken(user)
+            };
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<LoggedUserDto>> Login(LoginDto loginDTO)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.Username == loginDTO.Username);
+
+            if (user == null)
+            {
+                return Unauthorized("invalid username");
+            }
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i])
+                    return Unauthorized("invalid password");
+            }
+
+            return new LoggedUserDto
+            {
+                Username = user.Username,
+                Token = _tokenService.CreateToken(user)
+            };
+        }
+
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.Username == username.ToLower());
+        }
+    }
+}
---
+++ Backend/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Backend.Data;
+using Backend.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            this._context = context;
+        }
+
+        //[Authorize(Roles="Admin")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetAllUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+    }
+}
